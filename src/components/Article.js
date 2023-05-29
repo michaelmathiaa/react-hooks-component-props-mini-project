a651be9edc2a48443aac20b50f973a5504864d7e
Michael Mathias <michaelscodingjourney@gmail.com>
2023-05-29 00:58:47 -0700

    Added props to Article

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Article({ title, preview, date = "January 1, 1970"}) {
+function Article({ title, preview, minutes, date = "January 1, 1970"}) {
     //console.log(props);
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date} . {minutes} min read</small>
             <p>{preview}</p>
         </article>
     )
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,10 +2,9 @@ import React from "react";
 import Article from "./Article";
 
 function ArticleList({ posts }) {
-    //console.log(post);
     return (
         <main>
-            {posts.map(content => <article key = {content.id}></article>) }
+            {posts.map(content => <Article key = {content.id} title = {content.title} preview = {content.preview} date = {content.date} minutes = {content.minutes}/>)}
         </main>
     );
 }
